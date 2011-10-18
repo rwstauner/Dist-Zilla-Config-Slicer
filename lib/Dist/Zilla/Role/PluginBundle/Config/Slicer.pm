e60b6e2342ae4d8a9180c2109c2b4eec7dd2b49c
Randy Stauner <randy@magnificent-tears.com>
2011-10-17 21:54:52 -0700

    Define bundle role to make config slicing transparent
    Wrap bundle_config and merge all the plugin configurations.

---
+++ lib/Dist/Zilla/Role/PluginBundle/Config/Slicer.pm
@@ -0,0 +1,72 @@
+# vim: set ts=2 sts=2 sw=2 expandtab smarttab:
+use strict;
+use warnings;
+
+package Dist::Zilla::Role::PluginBundle::Config::Slicer;
+# ABSTRACT: Pass Portions of Bundle Config to Plugins
+
+use Config::MVP::Slicer ();
+use Moose::Role;
+
+requires 'bundle_config';
+
+# TODO: around add_bundle => sub { ($self, $bundle, $payload) = @_; $slicer->merge([$bundle, _bundle_class($bundle), $payload || {}]);
+
+around bundle_config => sub {
+  my ($orig, $class, $section) = @_;
+
+  my @plugins = $orig->($class, $section);
+
+  my $slicer = Dist::Zilla::Config::Slicer->new({
+    config => $section->{payload},
+  });
+
+  $slicer->merge($_) for @plugins;
+
+  return @plugins;
+};
+
+1;
+
+=head1 SYNOPSIS
+
+  # in your Dist::Zilla::PluginBundle::Example::Foo
+  with (
+    'Dist::Zilla::Role::PluginBundle', # or PluginBundle::Easy
+    'Dist::Zilla::Role::PluginBundle::Config::Slicer'
+  );
+  # Config::Slicer should probably be last (unless you're doing something complex)
+
+=head1 DESCRIPTION
+
+This role enables your
+L<Dist::Zilla::PluginBundle|Dist::Zilla::Role::PluginBundle>
+to accept configuration customizations for the plugins it will load
+and merge them transparently.
+
+  # dist.ini
+  [@Example::Foo]
+  option = 1
+  Included::Plugin.attribute = some value
+
+This role adds a method modifier to C<bundle_config>,
+which is the method that the root C<PluginBundle> role requires,
+and that C<PluginBundle::Easy> wraps.
+
+After C<bundle_config> is called
+the modifier will update the returned plugin configurations
+with any values that were customized in the main bundle config.
+
+Most of the work is done by L<Config::MVP::Slicer>.
+Check out that module if you want the same functionality
+but don't want to consume this role in your bundle.
+
+=head1 SEE ALSO
+
+=for :list
+* L<Config::MVP::Slicer>
+* L<Dist::Zilla>
+* L<Dist::Zilla::Role::PluginBundle>
+* L<Dist::Zilla::Role::PluginBundle::Easy>
+
+=cut
